61e23c832f3eb1745572f0fd144f9cea8ba1a7b2
CesarxArteaga <ctmces94@gmail.com>
2020-01-08 23:16:03 -0500

    Navbar Menu con opciones y botones del home

--- app/src/main/java/com/moviles/kiari/Home.java
+++ app/src/main/java/com/moviles/kiari/Home.java
@@ -1,8 +1,14 @@
 package com.moviles.kiari;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        Toolbar navbar_home = findViewById(R.id.nav_home);
+        setSupportActionBar(navbar_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menu_options = getMenuInflater();
+        menu_options.inflate(R.menu.menu_options,menu);
+        return true;
     }
 }
